ffd7fc1516d79f4435b6915270dcecf70331a8e5
futabario <futabario52@gmail.com>
2025-02-07 23:52:19 +0800

    12

--- riobot/plugins/remind/__init__.py
+++ riobot/plugins/remind/__init__.py
@@ -167,7 +167,14 @@ async def handle_reminder_set(bot: Bot, event: Union[MessageEvent, GroupMessageE
                     hour=hour,
                     minute=minute,
                     args=(new_id,),
-                    id=new_id
+                    id=new_id,
+                    kwargs={  # 新增此行
+                        "targets": targets,
+                        "content": content,
+                        "creator": event.user_id,
+                        "group_id": group_id,
+                        "time": f"{hour:02}:{minute:02}"
+                    }
                 )
                 job_id = new_id
 
@@ -251,18 +258,17 @@ async def startup():
     # 加载已有任务
     for job in scheduler.get_jobs():
         if job.id.startswith('rem_'):
-            print(job)
-            print(job.kwargs)
-
+            kwargs = job.kwargs
+            print(kwargs)
             try:
                 _, gid, time_part, _, _ = job.id.split('_', 4)
                 reminder_jobs[job.id] = {
                     "type": "group",
                     "group_id": int(gid),
-                    "targets": job.kwargs.get('targets', []),
-                    "content": job.kwargs.get('content', ''),
+                    "targets": kwargs.get('targets'),
+                    "content": kwargs.get('content',''),
                     "time": f"{time_part[:2]}:{time_part[2:4]}",
-                    "creator": job.kwargs.get('creator', 0)
+                    "creator": kwargs.get('creator', 0)
                 }
                 print(job.id)
                 print(reminder_jobs[job.id])
